bd5ad0ff972380f94987a52f963366764bc1364c
lunksana <zoufeng4@gmail.com>
2021-03-19 23:27:46 +0800

    首次尝试

--- db.py
+++ db.py
@@ -1,5 +1,23 @@
 import pymongo
 
-db = pymongo.MongoClient()
 class db_call():
-    def __init__(self,db_name,db_col,)
+    def __init__(self,db_addr,db_port,db_name,db_col,data):
+        self.db_addr = db_addr
+        self.db_port = db_port
+        self.db_name = db_name
+        self.db_col = db_col
+        self.data = data
+    
+    def link_db(self):
+        db_client = pymongo.MongoClient("mongodb://{self.db_addr}/{self.db_port}/")
+        db = db_client[self.db_name]
+        col = db[self.db_col]
+        return col
+    
+    def insert_data(self):
+        link_db().insert_many(self.data)
+        print("all data inserted!")
+    
+    def db_search(self)
+        pass
+
--- start.py
+++ start.py
@@ -1,2 +1,16 @@
 import json
+import ccxt
 import time
+
+class autotranding():
+    def __init__(self,order_symbol,order_type,order_price,order_side,num,exchange):
+        self.order_symbol = order_symbol
+        self.order_type = order_type
+        self.order_price = order_price
+        self.order_side = order_side
+        self.num = num
+        self.exchange = exchange
+    
+    def init_exchange(self):
+        bn = ccxt.binance()
+        return bn

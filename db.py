9ee2168add7172cde3e0a1d036cc7d57e7e4a718
lunksana <zoufeng4@gmail.com>
2021-03-22 00:14:38 +0800

    db test

--- db.py
+++ db.py
@@ -1,23 +1,27 @@
 import pymongo
 
-class db_call():
-    def __init__(self,db_addr,db_port,db_name,db_col,data):
+class Newdb():
+    def __init__(self,db_addr,db_port,db_name,db_col):
         self.db_addr = db_addr
         self.db_port = db_port
         self.db_name = db_name
         self.db_col = db_col
+    
+    def init_db(self):
+        self.db_client = pymongo.MongoClient(self.db_addr,self.db_port)
+        self.db = self.db_client[self.db_name]
+        self.col = self.db[self.db_col]
+        self.col.insert_one({'key': 1,"value": 'test1'})
+        
+    
+class Controldb():
+    def __init__(self,db,data):
+        self.db = db
         self.data = data
     
-    def link_db(self):
-        db_client = pymongo.MongoClient("mongodb://{self.db_addr}/{self.db_port}/")
-        db = db_client[self.db_name]
-        col = db[self.db_col]
-        return col
-    
-    def insert_data(self):
-        link_db().insert_many(self.data)
-        print("all data inserted!")
-    
-    def db_search(self)
+    def db_insert(self):
         pass
 
+mydb = Newdb('192.168.0.5',27017,'mytest','test1')
+print(mydb)
+print(type(mydb))

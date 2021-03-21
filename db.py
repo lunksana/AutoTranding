56efe2012033f926849cf8dc175d96c522e76c8c
lunksana <zoufeng4@gmail.com>
2021-03-22 00:29:22 +0800

    db_init_test

--- db.py
+++ db.py
@@ -8,10 +8,10 @@ class Newdb():
         self.db_col = db_col
     
     def init_db(self):
-        self.db_client = pymongo.MongoClient(self.db_addr,self.db_port)
-        self.db = self.db_client[self.db_name]
-        self.col = self.db[self.db_col]
-        self.col.insert_one({'key': 1,"value": 'test1'})
+        db_client = pymongo.MongoClient(self.db_addr,self.db_port)
+        db = db_client[self.db_name]
+        col = db[self.db_col]
+        return col.insert_one({'key': 1,"value": 'test1'})
         
     
 class Controldb():
@@ -24,4 +24,5 @@ class Controldb():
 
 mydb = Newdb('192.168.0.5',27017,'mytest','test1')
 print(mydb)
-print(type(mydb))
+print(mydb.init_db())
+print(mydb.__dict__)

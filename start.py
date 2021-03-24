9e31451d684763eca0d73b24d9af69ac47b21264
lunksana <zoufeng4@gmail.com>
2021-03-25 00:58:31 +0800

    加入配置判断

--- start.py
+++ start.py
@@ -17,12 +17,28 @@ class Autotranding():
         self.price_track = price_track
         self.max_leverage = max_leverage
     
-    def start(self):
+    def firststart(self,user_api,user_secret):
+        sys_conf = {
+            'position_control': self.position_control,
+            'sl_ratio': self.sl_ratio,
+            'price_track': self.price_track,
+            'max_leverage': self.max_leverage
+        }
+        user_api = {
+            'apiKey': user_api,
+            'secret': user_secret,
+        }
+    
+    def normalstart():
         pass
-        
-if __name__ = "__main__":
-    position_control = input("请输入仓位划分数量：")
-    sl_ratio = input("请输入止损比例：")
-    price_track = input("请输入价格追踪：")
-    max_leverage = input("请输入最大杠杆值：")
-    autotranding = Autotranding()
+
+if __name__ == '__main__':
+    if control.Json_ctr().json_read()['configured']:
+        position_control = input("请输入仓位划分数量：")
+        sl_ratio = input("请输入止损比例：")
+        price_track = input("请输入价格追踪：")
+        max_leverage = input("请输入最大杠杆值：")
+        user_api = input("请输入币安API：")
+        user_secret = input("请输入币安SECRET：")
+    else:
+        autotranding = Autotranding()

f9e0fcca29a6b193265c180f202ab5781c16176d
ksana410 <ddxiong0410@gmail.com>
2021-04-29 16:59:57 +0800

    20210429-001

--- auto.py
+++ auto.py
@@ -597,13 +597,13 @@ def Autotrading(side):
                     return
                 else:
                     if bn.fetch_ticker(symbol)['last'] < check_positions()[side]['pos_price'] - check_positions()[side]['pos_price'] * 0.25 / check_positions()[side]['pos_lev']:
-                        quick_order = create_tpsl_order('STOP_MARKET', None, None, side) #快速止损
+                        create_tpsl_order('STOP_MARKET', None, None, side) #快速止损
                         return
                     else:
                         price_step = avg_ch('5m')
                         pos_lev = check_positions()[side]['pos_lev']
                         limit_price = pos_price + price_step * (defense_count + 1)
-                        trigger_price = limit_list - price_step * defense_count
+                        trigger_price = limit_price - price_step * defense_count
                         sl_price = pos_price - pos_price * 0.25 / pos_lev
                         if alert_order == None:
                             alert_order = create_tpsl_order('STOP', 1, sl_price, side) #25%止损单
@@ -612,16 +612,14 @@ def Autotrading(side):
                             if btc_price < limit_price:
                                 defense_price = trigger_price - price_step
                                 if defense_price not in defense_order_dict.keys():
-                                    defense_order = create_tpsl_order('TAKE_PROFIT', 1, tmp_price, side) #防守订单
+                                    defense_order = create_tpsl_order('TAKE_PROFIT', 1, defense_price, side) #防守订单
                                     defense_count += 1
                                     defense_order_dict[defense_price] = defense_order
                                 time.sleep(5)
                             else:
-                                limit_price += price_step
+                                limit_price += avg_ch('15m')
                                 trigger_price += price_step
                                 time.sleep(5)
-                        else:
-                            pos_price = limit_price
         sl_price = pos_price / (1 - 0.25 / pos_lev)
     
     

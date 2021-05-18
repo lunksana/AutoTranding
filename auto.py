e6e7c0eb9e99d1c626b585e3c35c77f4747fb8b8
ksana410 <ddxiong0410@gmail.com>
2021-05-18 15:48:44 +0800

    删除错误

--- auto.py
+++ auto.py
@@ -5,8 +5,6 @@
 # 4.合理使用mongodb的查询功能
 # 5.定时任务参照 https://lz5z.com/Python%E5%AE%9A%E6%97%B6%E4%BB%BB%E5%8A%A1%E7%9A%84%E5%AE%9E%E7%8E%B0%E6%96%B9%E5%BC%8F/
 
-
-from typing import get_args
 import ccxt
 import time
 import pymongo

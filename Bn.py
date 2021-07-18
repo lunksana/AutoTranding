9c3983456f8d9c9299b32be58d2ff897c346f35e
lunksana <zoufeng4@gmail.com>
2021-07-18 21:25:34 +0800

    增加新的方法

--- Bn.py
+++ Bn.py
@@ -3,8 +3,6 @@
 #import ws
 import time
 import requests
-from requests.api import request
-import userapi
 import hashlib
 import hmac
 import re
@@ -33,7 +31,7 @@ class Bn:
         self.timeout = timeout
         self.headers = {'X-MBX-APIKEY': self.api_key}
     
-    def  _generate_signature(self, params: dict):
+    def _generate_signature(self, params: dict):
         query_str = urlencode(params)
         return hmac.new(self.secret.encode('utf-8'), query_str.encode('utf-8'), hashlib.sha256).hexdigest()
     
@@ -275,236 +273,31 @@ class Bn:
         else:
             return requests_data
         
-if __name__ == '__main__':
-    print(Bn.base_url)
-    bn = Bn('BTCUSDT', userapi.apiKey, userapi.secret)
-    pprint(bn.fetch_free_balance())
-    pprint(bn.cancel_all_orders())
-    
-exit()
-
-class Posctl:
-    def __init__(self, sym = None):
-        if sym:
-            self.sym = sym
-        else:
-            self.sym = 'BTCUSDT'
-    
-    def place_order(self, param):
-        return bn.fapiPrivate_post_order(param)
-    
-    def get_pos(self, pos_side):
-        pos_list = bn.fapiPrivateV2GetPositionRisk({'symbol': self.sym})
-        for i in pos_list:
-            if i['positionSide'] == pos_side:
-                return i
-
-    def create_pos(self, btc_price, btc_amt, order_type):
-        if btc_amt > 0:
-            side = 'BUY'
-            pos_side = 'LONG'
-        else:
-            side = 'SELL'
-            pos_side = 'SHORT'
-            btc_amt = abs(btc_amt)
-        if order_type not in ['LIMIT', 'MARKET']:
-            return
-        param = {
-            'symbol': self.sym,
-            'side': side,
-            'positionSide': pos_side,
-            'type': order_type,
-            'quantity': btc_amt
-        }
-        if order_type == 'LIMIT':
-            param['timeInForce'] = 'GTC'
-            param['price'] = btc_price
-        order_res = self.place_order(param)
-        if order_res['order_id']:
-            return order_res['order_id']
-    
-    def stop_pos(self):
-        pass
-
-    def cancel_order(self):
-        pass
-    
-    def create_stop_order(self, btc_price, pos_side, order_type):
-        if order_type not in ['STOP', 'STOP_MARKET']:
+    def create_pos(self, type, price, amt):
+        if type not in ['LIMIT', 'MARKET']:
             return
         else:
-            param = {
-                'symbol': self.sym,
-                'positionSide': pos_side,
-                'type': order_type, 
-                'stopPrice': btc_price,
-                'workingType': 'MARK_PRICE'
-            }
-        if order_type == 'STOP_MARKET':
-            param['closePosition'] = True
+            if amt > 0:
+                positionSide = 'LONG'
+                amt = round(amt, 3)
+            else:
+                positionSide = 'SHORT'
+                amt = round(abs(amt), 3)
+            pos_order = self.create_order(positionSide, type, amt, price, amt)
+        return pos_order['orderId']
+    
+    def create_stop_order(self, price, pos_side, amt = None):    
+        if amt:
+            type = 'STOP'
         else:
-            param['quantity'] = None
-        if pos_side == 'LONG':
-            side = 'SELL'
+            type = 'STOP_MARKET'
+        stop_order = self.create_order(pos_side, type, price, amt)
+        return stop_order['orderId']
+    
+    def create_tpsl_order(self, price, pos_side, amt = None):
+        if amt:
+            type = 'TAKE_PROFIT'
         else:
-            side = 'BUY'
-        param['side'] = side
-
-    def create_pfhl_order(self):
-        pass
-
-# def create_pos():
-#         def create_pos(self, data):
-#         sym = data['symbol']
-#         if data['price'] == '市价':
-#             res = brkrs.fetch_symbol_price(sym)
-#             price = float(res['price'])
-#             order_type = 'MARKET'
-#         else:
-#             price = float(data['price']) #fix price
-#             order_type = 'LIMIT'
-#         for an in data['vas']:
-#             self._set_lev(an, data)
-#             acct_equ = brkrs.all_equ[an]
-#             margin_rate = float(data['mr'][:-1])/100
-#             margin = acct_equ*margin_rate
-#             quote = margin*float(data['lev'])
-#             qp = brkrs.sym_infos[sym]['qp']
-#             qty = round(quote/price,qp)
-#             qty = max(qty,1/pow(10,qp))
-
-#             rstr = base62.encode(int(datetime.now().timestamp()))
-#             param = {
-#                 'symbol': sym,
-#                 'side': data['side'],
-#                 'positionSide': data['positionSide'],
-#                 'type': order_type,
-#                 'quantity': qty,
-#                 'newClientOrderId': f"{app}_{data['op']}_{rstr}", 
-#             }
-#             if order_type == 'LIMIT': 
-#                 param['price'] = price
-#                 param['timeInForce'] = 'GTC'
-#             res = brkrs.place_order(an, param)
-#             if res['orderId']:
-#                 op_ch = get_op(data['op'])
-#                 Lgr.log(data['op'],f"账户:{an} {op_ch}: {param}")
-#             else:
-#                 Lgr.log('ERROR','place order has error')
-
-#     def close_pos(self, data):
-#         sym = data['symbol']
-#         if data['price'] == '市价':
-#             res = brkrs.fetch_symbol_price(sym)
-#             price = float(res['price'])
-#             order_type = 'MARKET'
-#         else:
-#             price = float(data['price']) #fix price
-#             order_type = 'LIMIT'
-#         for an in data['vas']:
-#             qp = brkrs.sym_infos[sym]['qp']
-#             close_rate = float(data['cr'][:-1])/100
-#             pos_amt = 0
-#             for pos in brkrs.all_valid_pos[an]:
-#                 if pos['symbol'] == sym and pos['positionSide'] == data['positionSide']:
-#                     pos_amt = float(pos['positionAmt'])
-#             if not pos_amt:
-#                 Lgr.log('bnews', f'账户 {an} 没有找到对应仓位')
-#                 return
-#             qty = round(abs(pos_amt)*close_rate,qp)
-#             qty = max(qty,1/pow(10,qp))
-#             rstr = base62.encode(int(datetime.now().timestamp()))
-#             param = {
-#                 'symbol': sym,
-#                 'side': data['side'],
-#                 'positionSide': data['positionSide'],
-#                 'type': order_type,
-#                 'quantity': qty,
-#                 'newClientOrderId': f"{app}_{data['op']}_{rstr}", 
-#             }
-#             if order_type == 'LIMIT': 
-#                 param['price'] = price
-#                 param['timeInForce'] = 'GTC'
-#             res = brkrs.place_order(an, param)
-#             if res['orderId']:
-#                 op_ch = get_op(data['op'])
-#                 Lgr.log(data['op'],f"账户:{an} {op_ch}: {param}")
-#             else:
-#                 Lgr.log('ERROR','place order has error')
-
-
-#     def cancel_porders(self, porder_df, idxs):
-#         for idx in idxs.split(' '):
-#             order = porder_df.iloc[int(idx)]
-#             param = {
-#                 "symbol": order['symbol'],
-#                 "orderId": order['orderId']
-#             }
-#             res = brkrs.cancel_porder(order['账户'],param)
-#             if res and res['orderId']:
-#                 Lgr.log('SUCCESS',f"撤销订单 {res['symbol']} {res['clientOrderId']} 成功")
-
-
-#     def create_stop_order(self, data):
-#         sym = data['symbol']
-#         for an in data['vas']:
-#             rstr = base62.encode(int(datetime.now().timestamp()))
-#             param = {
-#                 'symbol': sym,
-#                 'side': data['side'],
-#                 'positionSide': data['positionSide'],
-#                 'type': 'STOP_MARKET',
-#                 'newClientOrderId': f"{app}_stplos_{rstr}", 
-#                 'stopPrice': data['price'],
-#                 'closePosition': True,
-#                 'workingType': 'MARK_PRICE'
-#             }
-#             res = brkrs.place_order(an, param)
-#             if res['orderId']:
-#                 op_ch = get_op(data['op'])
-#                 Lgr.log(data['op'],f"账户:{an} {op_ch}: {param}")
-#             else:
-#                 Lgr.log('ERROR','place order has error')
-
-#     # 下 LIMIT 止盈单
-#     def create_pfhl_order(self, data):
-#         stp_p = float(data['price'])
-#         sym = data['symbol']
-#         pside = data['positionSide']
-        
-#         for an in data['vas']:
-#             pp = brkrs.sym_infos[sym]['pp']
-#             qp = brkrs.sym_infos[sym]['qp']
-#             close_rate = 0.5
-#             pos_amt = 0
-#             brkrs.fetch_postion(an)
-#             for pos in brkrs.all_valid_pos[an]:
-#                 if pos['symbol'] == sym and pos['positionSide'] == pside:
-#                     pos_amt = float(pos['positionAmt'])
-#                     entry_p = float(pos['entryPrice'])
-#             if not pos_amt:
-#                 Lgr.log('bnews', f"账户 {an} {sym} 没有 {pside} 仓位")
-#                 continue
-#             if pside == 'LONG':
-#                 price = round(abs(entry_p-stp_p)+entry_p*(1+Settings['slippage']),pp)
-#             else:
-#                 price = round(entry_p*(1-Settings['slippage'])-abs(entry_p-stp_p),pp)
-#             qty = round(abs(pos_amt)*close_rate,qp)
-#             qty = max(qty,1/pow(10,qp))
-#             rstr = base62.encode(int(datetime.now().timestamp()))
-#             param = {
-#                 'symbol': sym,
-#                 'side': data['side'],
-#                 'positionSide': pside,
-#                 'price': price,
-#                 'type': 'LIMIT',
-#                 'timeInForce': 'GTC',
-#                 'quantity': qty,
-#                 'newClientOrderId': f"{app}_{data['op']}_{rstr}", 
-#             }
-#             res = brkrs.place_order(an, param)
-#             if res['orderId']:
-#                 op_ch = get_op(data['op'])
-#                 Lgr.log(data['op'],f"账户:{an} {op_ch}: {param}")
-#             else:
-#                 Lgr.log('ERROR','place order has error')
+            type = 'TAKE_PROFIT_MARKET'
+        tpsl_order = self.create_order(pos_side, type, amt, price)
+        return tpsl_order['orderId']            
